c92023d83abd8a21e5228a5cbd662ce657a66c8f
okeke chimezie glory <okekechimezieglory@gmail.com>
2024-10-07 21:37:07 +0000

    set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Chimezie Okeke <https://github.com/okekechimezieglory>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
